87e4359d76879b576877ff0da085751d8706a6e1
Patey Valentin <valentin.patey@etu.unicaen.fr>
2023-10-23 17:36:07 +0200

    :fix: requête Sql et maj de la var. session

--- templates/traitementConnexion.php
+++ templates/traitementConnexion.php
@@ -1,22 +1,21 @@
 <?php
 
+if(session_id() == '') {
+    session_start();
+}
 if (
     isset($_POST['pseudo']) && isset($_POST['motDePasse']) && !empty($_POST['pseudo'])
     && !empty($_POST['motDePasse'])
 ) {
+    require('connexion.php');
     $pseudo = $_POST['pseudo'];
     $motDePasse = $_POST['motDePasse'];
-    $reponse = $bdd->query(
-        'select count(*) as nb from utilisateur where (pseudo = ' . $pseudo . ' and mot_de_passe = ' . $motDePasse . ')
-        or (adresse_mail = ' . $pseudo . ' and mot_de_passe = ' . $motDePasse . ')'
-    );
-    require('connexion.php');
+    $sql = "select count(*) as nb from UTILISATEUR where (pseudo = '" . $pseudo . "' and mot_de_passe = '" . $motDePasse . "')
+        or (adresse_mail = '" . $pseudo . "' and mot_de_passe = '" . $motDePasse . "')";
+    $reponse = $bdd-> query($sql);
     $donnees = $reponse->fetch();
     $nb = $donnees['nb'];
-    if ($nb = 1) {
-        if(session_id() == '') {
-            session_start();
-        }
+    if ($nb == 1) {
         if ($pseudo == 'admin') {
             $_SESSION['userType'] = 0;
         } else {
@@ -24,7 +23,7 @@ if (
         }
     }
     else {
-        header('Location: formConnexion.php');
+        $_SESSION['userType'] = '';
     }
 } else {
     echo "unset";

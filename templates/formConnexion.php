f7d4644b2b0faf77fdb03b03eb4dc3f46efa1109
Patey Valentin <valentin.patey@etu.unicaen.fr>
2023-10-23 16:31:59 +0200

    Ajout du form et du traitement de connexion

---
+++ templates/formConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="traitementConnexion.php" method="post">
+        Pseudo ou Email :
+        <input type="text" name="pseudo">
+        Mot de passe :
+        <input type="text" name="motDePasse">
+        <input type="submit" value="Se Connecter">
+    </form>
+</body>
+
+</html>
---
+++ templates/traitementConnexion.php
@@ -0,0 +1,29 @@
+<?php
+if (
+    isset($_POST['pseudo']) && isset($_POST['motDePasse']) && !empty($_POST['pseudo'])
+    && !empty($_POST['motDePasse'])
+) {
+    $pseudo = $_POST['pseudo'];
+    $motDePasse = $_POST['motDePasse'];
+    $reponse = $bdd->query(
+        'select count(*) as nb from utilisateur where (pseudo = ' . $pseudo . ' and mot_de_passe = ' . $motDePasse . ')
+        or (adresse_mail = ' . $pseudo . ' and mot_de_passe = ' . $motDePasse . ')'
+    );
+    $donnees = $reponse->fetch();
+    $nb = $donnees['nb'];
+    if ($nb = 1) {
+        if(session_id() == '') {
+            session_start();
+        }
+        if ($pseudo == 'admin') {
+            $_SESSION['userType'] = 1;
+        } else {
+            $_SESSION['userType'] = 0;
+        }
+    }
+    else {
+        header('Location: formConnexion.php');
+    }
+} else {
+    echo "unset";
+}

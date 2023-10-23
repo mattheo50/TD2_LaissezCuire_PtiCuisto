3236c9b6ae2f07ae05de45136cbb298516720d50
aubry224 <aubry224@unicaen.fr>
2023-10-23 09:58:37 +0200

    Edito créé

---
+++ templates/edito.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <header>
+            <?php
+                include("./navbar.php");
+            ?>
+        </header>
+
+        <main>
+            <section id="bigDiv">
+                Des Trucs
+            </section>
+
+            <section id="alignSect">
+                <section id="sectionLast">
+                    <h2>Les dernières recettes</h2>
+                    <div id="lastRecipes">
+                        <div class="lastRecipe">
+                            <img src="/images/Pticuisto.png" alt="Petit Cuistot">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                               elit. Duis sodales commodo sapien, eget egestas diam
+                               vulputate sit amet.</p>
+                        </div>
+                        <div class="lastRecipe">
+                            <img src="/images/Pticuisto.png" alt="Petit Cuistot">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                               elit. Duis sodales commodo sapien, eget egestas diam
+                               vulputate sit amet.</p>
+                        </div>
+                        <div class="lastRecipe">
+                            <img src="/images/Pticuisto.png" alt="Petit Cuistot">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                               elit. Duis sodales commodo sapien, eget egestas diam
+                               vulputate sit amet.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="edito">
+                    <img id="imgCuistot" src="/images/Pticuisto.png" alt="Petit Cuistot">
+                    <h2>Edito</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                       elit. Duis sodales commodo sapien, eget egestas diam
+                       vulputate sit amet. Duis ut semper magna, eu lobortis
+                       sem. In mollis lectus dolor, vitae tincidunt tellus
+                       hendrerit eu. Sed consequat diam sed orci fringilla
+                       dapibus. Etiam sed leo eget metus facilisis tristique
+                       eget sit amet risus. Cras purus risus, interdum porta
+                       lorem venenatis, suscipit pulvinar mauris.</p>
+                </section>
+            </section>
+        </main>
+        <footer>
+            <div id="socialLinks">
+                
+            </div>
+        </footer>
+    </body>
+</html>

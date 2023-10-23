1642d9314e39adbe4c709f841810c90287ebcb09
Patey Valentin <valentin.patey@etu.unicaen.fr>
2023-10-23 16:45:16 +0200

    ajout du lien avec la connexion BDD

--- templates/traitementConnexion.php
+++ templates/traitementConnexion.php
@@ -1,4 +1,5 @@
 <?php
+
 if (
     isset($_POST['pseudo']) && isset($_POST['motDePasse']) && !empty($_POST['pseudo'])
     && !empty($_POST['motDePasse'])
@@ -9,6 +10,7 @@ if (
         'select count(*) as nb from utilisateur where (pseudo = ' . $pseudo . ' and mot_de_passe = ' . $motDePasse . ')
         or (adresse_mail = ' . $pseudo . ' and mot_de_passe = ' . $motDePasse . ')'
     );
+    require('connexion.php');
     $donnees = $reponse->fetch();
     $nb = $donnees['nb'];
     if ($nb = 1) {

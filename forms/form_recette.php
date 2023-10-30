d94dee531015dbb8790ce27bc35bb47e5b04f62a
Nicolle Raphaël <batraphlaly@gmail.com>
2023-10-30 14:38:25 +0100

    feat/ tableau ingredients

--- forms/form_recette.php
+++ forms/form_recette.php
@@ -1,28 +1,32 @@
 <?php include_once("../templates/navbar.php")?>
 <html>
     <body>
-    <form action="traitement_form_recette.php" method="post">
-        <label>Quelle est la catégorie de votre recette ? </label>
-        <select name="categorie" id="categorie_recette_form_recette">
-            <option value="">--Choississez une catégorie--</option>
-            <option value="Entree">Entrée</option>
-            <option value="Plat">Plat</option>
-            <option value="Dessert">Dessert</option>
-        </select><br>
-        <label>Entrez votre titre: </label>
-        <input name="titre" type="text" placeholder="Titre"></input><br>
-        <label>Entrez la recette: </label>
-        <input name="contenu" type="text" placeholder="recette"></input><br>
-        <label>Saisissez un court résumé de la recette: </label>
-        <input name="resume" type="text" placeholder="résumé"></input><br>
-        
-        <form action="autocomplete.php" method="post">
-        <label for="Séléctionnez les ingrédients de la recette:">Séléctionnez les ingrédients de la recette: </label>
-        <input type="search" id="ingredient" name="ingredient" list="Listingredient" autocomplete="off">
-        <button id="ajout_ing_bouton" type="button" >+</button>
-        <datalist id="Listingredient"></datalist>
-        <p id="ListeIngredientAjoute">Ingredients : </p>
-    </form>
+        <form action="traitement_form_recette.php" method="post">
+            <label>Quelle est la catégorie de votre recette ? </label>
+            <select name="categorie" id="categorie_recette_form_recette">
+                <option value="">--Choississez une catégorie--</option>
+                <option value="Entree">Entrée</option>
+                <option value="Plat">Plat</option>
+                <option value="Dessert">Dessert</option>
+            </select><br>
+            <label>Entrez votre titre: </label>
+                <input name="titre" type="text" placeholder="Titre"></input><br>
+            <label>Entrez la recette complète: </label><br>
+                <textarea name="contenu" type="text" rows="10" cols="100" placeholder="recette"></textarea><br>
+            <label>Saisissez un court résumé de la recette: </label><br>
+            <textarea name="resume" type="text" placeholder="résumé" rows="4" cols="30"></textarea><br>
+            <label for="Séléctionnez les ingrédients de la recette:">Séléctionnez les ingrédients de la recette: </label>
+                <input type="search" id="ingredient" name="ingredient" list="Listingredient" autocomplete="off">
+            <button id="ajout_ing_bouton" type="button" onclick="resetbouton()" >+</button>
+            <datalist id="Listingredient"></datalist><br>
+            <label>Ingredients :</label>
+            <label id="ListeIngredientAjoute"></label><br>
+            <input type="hidden" id="ingredientPost" name="ingredientPost" >
+            <label>Saisissez un lien d'image: </label>
+                <input name="image" type="text" placeholder="Liens"></input><br>
+                <input type="submit" value="Send Request"/>
+        </form>
+    </body>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         let recherche;
@@ -45,24 +49,29 @@
                 }
             });
         });
-        console.log(recherche);
-        
+        let ListeIngredientAjouteTab = [];
         const ajoutIngredient = document.getElementById("ajout_ing_bouton");
         let premierIngredient = true;
         ajoutIngredient.addEventListener("click",(event)=>{
-            if(premierIngredient == true){
-                document.getElementById("ListeIngredientAjoute").innerHTML += (recherche);
-                premierIngredient = false;
-            }else{
-                document.getElementById("ListeIngredientAjoute").innerHTML += (", "+recherche);
+            if(!(recherche == undefined)){
+                if(premierIngredient == true){
+                    document.getElementById("ListeIngredientAjoute").innerHTML += recherche;
+                    document.getElementById("ingredientPost").value += recherche;
+                    premierIngredient = false;
+                }else{
+                    document.getElementById("ListeIngredientAjoute").innerHTML += (", "+recherche);
+                    document.getElementById("ingredientPost").value += "/"+recherche;
+                }
+                ListeIngredientAjouteTab.push(recherche);
+                console.log(document.getElementById("ingredientPost").value);
             }
         });
-    </script>
-       
 
-        <label>Saisissez un lien d'image: </label>
-        <input name="image" type="text" placeholder="Titre"></input><br>
-        <input type="submit" value="Send Request" />
-    </form>
-    </body>
+        function resetbouton(){
+            var bouton = document.getElementById("ajout_ing_bouton");
+            bouton.value = undefined;
+            //chercher a faire plus propre
+        }
+
+    </script>
 </hmtl>
--- forms/traitement_form_recette.php
+++ forms/traitement_form_recette.php
@@ -1,5 +1,28 @@
 <?php include_once("../templates/navbar.php")?>
 <?php
-echo($_POST['titre']);
+echo ("<pre>");
+echo(print_r($_POST));
+echo ("</pre>");
+
+$categorie = strip_tags($_POST["categorie"]);
+echo (print("$categorie"));
+$titre = strip_tags($_POST["titre"]);
+echo (print("$titre"));
+$contenu = strip_tags($_POST["contenu"]);
+echo (print("$contenu"));
+$resume = strip_tags($_POST["resume"]);
+echo (print("$resume"));
+$ingredientPost = strip_tags($_POST["ingredientPost"]);
+echo (print("$ingredientPost"));
+$image = strip_tags($_POST["image"]);
+echo (print("$image"));
+$rec_num;
+$tag_num;
+$uti_num = isset($_SESSION['uti_num']);
+
+
+
+$insert_recette = "insert into RECETTE(REC_NUM,TAG_NUM,CAT_NUM,UTI_NUM,TITRE,CONTENU,RESUME,DATE_CREATION,IMAGE) values($rec_num,$tag_num,'$categorie',$uti,'$titre','$contenu','$resume',sysdate,'$image')";
+echo (print("$insert_recette    "));
 
 ?>

885098a09b97651e79d6b0ace160b069a5f420b9
PateyV <valentin.patey@etu.unicaen.fr>
2023-11-06 11:50:59 +0100

    début template recette + requêtes sql

---
+++ templates/recette.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recette</title>
+</head>
+<body>
+    <p>TAG CAT UTI_NUM TITRE CONTENU RESUME DATECREA DATEMODIF IMAGE</p>
+    <h2>"Titre"</h2>
+    <p id="rectte_cat">"Cat"</p>
+
+    <img alt="image de la recette"></img>
+
+    <p>"Résumé"</p>
+
+    <p>ingrédients</p>
+
+    <p>"Contenu"</p>
+
+    <p>Crée par "uilisateur" le "date". Modifié le : "Date"</p>
+
+</body>
+</html>

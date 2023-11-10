4008ec5ef3024bbfbc94a9cb7e13968d5121860c
Nicolle Raphaël <batraphlaly@gmail.com>
2023-11-10 13:26:43 +0100

    feat : ajout ingredients auto

--- forms/Controller/Recette.php
+++ forms/Controller/Recette.php
@@ -1,6 +1,6 @@
 <?php
 
-require('config.php');
+require("config.php");
 
 Class Recette {
 
@@ -15,5 +15,50 @@ Class Recette {
         }
     }
 
+    public function inserer_recette($uti_num,$ingredientPost,$tags,$categorie, $titre, $contenu, $resume, $image){
+        require("config.php");
+        //récupération du numéro du prochain numero de recette
+        $max_rec_num = $bdd->query("select max(REC_NUM)+1 from RECETTE");
+        $rec_num = $max_rec_num->fetch();
+
+        //verification de la validité des ingrédients
+        $ingredientPost = explode("/",$ingredientPost);
+        $erreur = false;
+        $ing_num[] = array();
+        for($i=0;$i < count($ingredientPost); $i++){
+            // je vais chercher le numéro de l'ingrédient avec le nom entré
+            //en vérifiant si l'ingrédient existe
+            $ingredient = null;
+            $select_ing_num = "SELECT ING_NUM from INGREDIENT WHERE INTITULE_ING = ?";
+            $statement = $bdd->prepare($select_ing_num);
+            $statement->execute([$ingredientPost[$i]]);
+            $ingredient = $statement->fetch();
+            if($ingredient == false){
+                $max_ing_num = $bdd->query("select max(ing_NUM)+1 from INGREDIENT");
+                $ing_num_new = $max_ing_num->fetch();
+                $insert_ingredient = "INSERT INTO INGREDIENT(ING_NUM,INTITULE_ING) VALUES(?,?)";
+                $statement = $bdd->prepare($insert_ingredient);
+                $statement->execute([$ing_num_new[0],$ingredientPost[$i]]);
+                $ingredient = $ing_num_new[0];
+                array_push($ing_num,$ingredient);
+            }else{
+                array_push($ing_num,$ingredient['ING_NUM']);
+            }   
+        }
+
+        // Insertion dans recette
+        $insert_recette = "INSERT INTO RECETTE(REC_NUM, TAG_NUM, CAT_NUM, UTI_NUM, TITRE, CONTENU, RESUME, DATE_CREATION, IMAGE) VALUES (?, ?, ?, ?, ?, ?, ?, sysdate(), ?)";
+        $statement = $bdd->prepare($insert_recette);
+        $statement->execute([$rec_num[0], $tags, $categorie, $uti_num, $titre, $contenu, $resume, $image]);
+        echo "votre recette est en train d'être étudier si elle ne contient rien contraire aux règles d'utilisation elle sera bientôt disonible sur notre site !";
+
+        //Serie d'insertions dans la table composer
+        for($i=1;$i < count($ing_num); $i++){
+            //j'insere le numéro d'ingrédient à la recette dans la table composer/*
+            $insert_composer = "INSERT INTO COMPOSER(REC_NUM, ING_NUM) VALUES (?, ?)";
+            $statement = $bdd->prepare($insert_composer);
+            $statement->execute([$rec_num[0],$ing_num[$i]]);
+        } 
+    }
 
 }
--- forms/traitement_form_recette.php
+++ forms/traitement_form_recette.php
@@ -1,4 +1,4 @@
-<?php include_once("../templates/navbar.php"); require("config.php");
+<?php include_once("../templates/navbar.php");require('Controller/Recette.php');
 
 //recuperation des donnees via POST
 $categorie = strip_tags($_POST["categorie"]);
@@ -14,58 +14,18 @@ $resume = strip_tags($_POST["resume"]);
 $ingredientPost = strip_tags($_POST["ingredientPost"]);
 
 //non obligatoire
-if(isset($_SESSION['image'])){
+if(isset($_POST['image'])){
     $image = $_POST['image'];
 }else{
-    $image = "";
+    $image = "https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png";
 };
 
-//récupération du numéro du prochain numero de recette
-$max_rec_num = $bdd->query("select max(REC_NUM)+1 from RECETTE");
-$rec_num = $max_rec_num->fetch();
 
-//temporaire à retirer !!!!!!!!
-if(isset($_SESSION['uti_num'])){
-    $uti_num = $_SESSION['uti_num'];
-}else{
-    $uti_num = 1;
-};
+$uti_num = $_SESSION['uti_num'];
 
-//verification de la validité des ingrédients
-$ingredientPost = explode("/",$ingredientPost);
-$erreur = false;
-for($i=0;$i < count($ingredientPost); $i++){
-    // je vais chercher le numéro de l'ingrédient avec le nom entré
-    //en vérifiant si l'ingrédient existe
-    try{
-        $select_ing_num = "SELECT ING_NUM from INGREDIENT WHERE INTITULE_ING = ?";
-        $statement = $bdd->prepare($select_ing_num);
-        $statement->execute([$ingredientPost[$i]]);
-        $ingredient = $statement->fetch();
-        $ing_num[];
-        array_push($ing_num,$ingredient["ING_NUM"]);
-    }catch(PDOException $e){
-        $erreur = true;
-    }
-}
 
-// Si l'ingrédient n'existe pas on ne peut pas l'insérer
-if($erreur = true){
-    echo "Un problème s'est produit lors de la vérification des ingrédients de la recette";
-}else{
-    // Insertion dans recette
-    $insert_recette = "INSERT INTO RECETTE(REC_NUM, TAG_NUM, CAT_NUM, UTI_NUM, TITRE, CONTENU, RESUME, DATE_CREATION, IMAGE) VALUES (?, ?, ?, ?, ?, ?, ?, sysdate(), ?)";
-    $statement = $bdd->prepare($insert_recette);
-    $statement->execute([$rec_num[0], $tags, $categorie, $uti_num, $titre, $contenu, $resume, $image]);
-    echo "votre recette est en train d'être étudier si elle ne contient rien contraire aux règles d'utilisation elle sera bientôt disonible sur notre site !";
+$recette = new Recette();
 
-    //Serie d'insertions dans la table composer
-    for($i=0;$i < count($ing_num); $i++){
-        //j'insere le numéro d'ingrédient à la recette dans la table composer/*
-        $insert_composer = "INSERT INTO COMPOSER(REC_NUM, ING_NUM) VALUES (?, ?)";
-        $statement = $bdd->prepare($insert_composer);
-        $statement->execute([$rec_num[0],$ing_num[$i]]);
-    }
-}
+$recette->inserer_recette($uti_num,$ingredientPost, $tags, $categorie,$titre, $contenu, $resume, $image);
 
 ?>

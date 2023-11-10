1b1ad6a590b9251d72ec848542ed15ae277ab393
Vireth <vireth4114@gmail.com>
2023-11-10 13:28:32 +0100

    CSS amélioré

--- vue/nosRecettes.php
+++ vue/nosRecettes.php
@@ -4,18 +4,25 @@
         <?php
         while($data = $recettes->fetch()){
             echo "<div class='recipe'>";
+            echo "<div id='divimg'>";
             echo "<img src='".$data['image']."' alt='Image recette'>";
+            echo "</div>";
             echo "<h4>".$data['titre']."</h4>";
             echo "</div>";
         }
         ?>
         </div>
 
+        <div id="divButton">
         <?php
-            if ($offset < $count - 10) {
-                echo "<a id='moreRecipes' href='index.php?action=nosRecettes&offset=".($_SESSION['offset'] + 10)."'>Afficher plus</a>";
+            if ($offset != 10) {
+                echo "<a class='ctrlRecipes' href='index.php?action=nosRecettes&offset=".($_SESSION['offset'] - 10)."'>Précédent</a>";
+            }
+            if ($offset < $count - 3) {
+                echo "<a class='ctrlRecipes' href='index.php?action=nosRecettes&offset=".($_SESSION['offset'] + 10)."'>Suivant</a>";
             }
         ?>
+        </div>
     </section>
 <?php $content = ob_get_clean();
 require("vue/template.php"); ?>

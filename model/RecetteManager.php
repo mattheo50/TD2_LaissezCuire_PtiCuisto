352f5a2533462f89405281f1e781d29577d0f16d
22013679 <mattheo.mouchel@gmail.com>
2023-11-10 10:47:50 +0100

    Corrige le recette manager avec l'ajout de verifie

--- model/RecetteManager.php
+++ model/RecetteManager.php
@@ -4,7 +4,7 @@ class RecetteManager extends Connexion{
     public function getDernieresRecettes()
     {
         $bdd = $this->dbConnect();
-        $req = "select titre, resume, image from RECETTE order by date_creation desc limit 3";
+        $req = "select titre, resume, image from RECETTE where verifie = 1 order by date_creation desc limit 3";
         $sql = $bdd -> prepare($req);
         $sql -> execute();
         return $sql;
